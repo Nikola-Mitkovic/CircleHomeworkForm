423b8fe43ec29d87f3d855692ae31fb9373d30de
Nikola-Mitkovic <ektim35@gmail.com>
2022-10-31 01:48:18 +0100

    something gone wrong and didn't want to push code on first try

---
+++ CircleHomeworkForm/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace CircleHomeworkForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblCircumference = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Radius (r)";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(227, 46);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 31);
+            this.txtRadius.TabIndex = 1;
+            this.txtRadius.Text = "0";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(84, 111);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(243, 90);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate Area and Circumference";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(79, 248);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(57, 25);
+            this.lblArea.TabIndex = 3;
+            this.lblArea.Text = "Area";
+            // 
+            // lblCircumference
+            // 
+            this.lblCircumference.AutoSize = true;
+            this.lblCircumference.Location = new System.Drawing.Point(79, 303);
+            this.lblCircumference.Name = "lblCircumference";
+            this.lblCircumference.Size = new System.Drawing.Size(151, 25);
+            this.lblCircumference.TabIndex = 4;
+            this.lblCircumference.Text = "Circumference";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 358);
+            this.Controls.Add(this.lblCircumference);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Circle Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblCircumference;
+    }
+}
+
---
+++ CircleHomeworkForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircleHomeworkForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double radius, area, circumference;
+            radius = Convert.ToDouble(txtRadius.Text);
+            
+            area = 3.14 * (radius * radius);
+            circumference = 2 * 3.14 * radius;
+
+            lblArea.Text = area.ToString();
+            lblCircumference.Text = circumference.ToString();
+        }
+    }
+}
